b6aaf4bb4838a68e849acbfd4e45c6d1d6fe9567
romarin0325-cell <romarin0325-cell@users.noreply.github.com>
2026-06-01 01:44:21 +0900

    feat: Add challenge hard mode

--- card/rpg_features.js
+++ card/rpg_features.js
@@ -1123,6 +1123,7 @@
         this.state = {
             mode: mode,
             gameType: this.tempGameType || 'challenge',
+            hardMode: !!this.hardModeActive,
             tickets: initTickets,
             inventory: [],
             deck: [null, null, null],
